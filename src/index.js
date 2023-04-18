57b64d847bd4ae1cd965d28ccc00688f69e0867a
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-18 01:50:00 -0700

    Add page-layout.js addContent function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles/index.css';
+import { addContent } from './modules/page-layout';
+const content = document.querySelector('#content');
+const sideBar = document.querySelector('#sidebar');
+
+addContent(sideBar, 'Hello', 'Message');
--- src/modules/page-layout.js
+++ src/modules/page-layout.js
@@ -1,1 +1,10 @@
 const content = document.querySelector('#content');
+
+const addContent = (parent, item, className) => {
+    const div = document.createElement('div');
+    div.textContent = item;
+    div.classList.add(className);
+    parent.appendChild(div);
+};
+
+export { addContent };

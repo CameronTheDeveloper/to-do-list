540a5e9792132f6e535a4afdd4ebf8deda030256
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-18 04:48:24 -0700

    Add get-user-input.js, new addSideBarContent()

--- src/modules/sidebar.js
+++ src/modules/sidebar.js
@@ -2,11 +2,16 @@ import { addContent } from './page-layout';
 const sideBar = document.querySelector('#sidebar');
 let sideBarContent = ['Projects', 'Today', 'This Week'];
 
+const addSideBarContent = () => {
+
+};
+
 const renderSideBarContent = () => {
     for (let i = 0; i < sideBarContent.length; i++) {
         addContent(sideBar, sideBarContent[i], 'sidebar-item');
     }
 };
+
 
 
 export { renderSideBarContent };

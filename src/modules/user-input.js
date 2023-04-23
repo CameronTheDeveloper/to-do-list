541a033b55e4b14cfdc3cb6d8847586acf0bf773
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-23 05:53:11 -0700

    Add user-input.js setSideBarInput

--- src/modules/user-input.js
+++ src/modules/user-input.js
@@ -1,4 +1,8 @@
+import { addAbove } from "./page-layout";
+
 const content = document.querySelector('#content');
+const sideBarItems = document.querySelector('#sidebar-items');
+const sideBarForm = document.querySelectorAll('#sidebar-add-form');
 
 const openSideBarForm = () => {
 
@@ -8,12 +12,15 @@ const getSideBarInput = () => {
     return document.getElementById('folder-title').value;
 };
 
+const setSideBarInput = (input) => {
+    addAbove(input, 'sidebar-item', 'div', sideBarItems, sideBarForm);
+};
+
 const registerSideBarSubmitListener = (form) => {
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         const input = getSideBarInput();
-
-        // Add input as folder
+        setSideBarInput(input);
     });
 };
 

85e46c1d2c76cd2f1fc857a74387ac69a52cccaf
CameronTheDeveloper <cameronposey@outlook.com>
2023-05-03 21:10:15 -0700

    Add clear-mode.js to clear todos on click

---
+++ src/modules/clear-mode.js
@@ -0,0 +1,10 @@
+// query selector - clear button;
+
+// clearbutton.addeventlistener click;
+
+// clearmode = true / false;
+
+// todo.addeventlistener click;
+// if clearmode == true{
+//     todo.removeHTML();
+// }

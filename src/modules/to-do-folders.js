8af5053633a46ec51120eeca671e2c2d6634fb62
CameronTheDeveloper <cameronposey@outlook.com>
2023-05-05 03:16:25 -0700

    Return folder in addToDoFolder

--- src/modules/to-do-folders.js
+++ src/modules/to-do-folders.js
@@ -4,6 +4,8 @@ const addToDoFolder = (folderName) => {
     const folder = document.createElement('div');
     folder.classList.add(folderName);
     folder.appendChild('#content-items');
+    return folder;
 };
 
+//In display-to-do (page-layout.js), replace to-do parent with folder
 //Clear folder when folder-remove button is clicked

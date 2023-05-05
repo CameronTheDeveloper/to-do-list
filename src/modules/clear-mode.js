03d4a3382286107199b4803179b75e9e7c78ed8d
CameronTheDeveloper <cameronposey@outlook.com>
2023-05-04 19:29:34 -0700

    Add clear-mode.js addClearEventListener

--- src/modules/clear-mode.js
+++ src/modules/clear-mode.js
@@ -1,10 +1,36 @@
-// query selector - clear button;
+import { removeHTML } from "./page-layout";
 
-// clearbutton.addeventlistener click;
+const clearButton = document.querySelector('#todo-clear-mode-button');
+let clearMode = false;
 
-// clearmode = true / false;
+clearButton.addEventListener('click', () => {
+    if (!clearMode) {
+        clearMode = true;
+    } else {
+        clearMode = false;
+    }
+});
+
+//Call on each to do created
+const addClearEventListener = (element) => {
+    element.addEventListener('click', () => {
+        if (clearMode) {
+            removeHTML(element);
+        } else {
+            //Expand to do
+        }
+    });
+
+};
+
+// toDos.forEach(todo => {
+
+// });
+
 
 // todo.addeventlistener click;
 // if clearmode == true{
 //     todo.removeHTML();
 // }
+
+export { addClearEventListener };

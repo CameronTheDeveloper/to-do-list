65fae8d164912f13ffcabdc4e618c255b2b0bc76
CameronTheDeveloper <cameronposey@outlook.com>
2023-05-10 06:46:40 -0700

    Remove sidebar.js renderSidebarContent, dead code

--- src/modules/sidebar.js
+++ src/modules/sidebar.js
@@ -1,21 +1,10 @@
 import '../styles/sidebar.css';
-import { addContent, addButton, addSidebarForm } from './page-layout';
-import { addToDoFolder } from './to-do-folders';
+import { addButton, addSidebarForm } from './page-layout';
 
 const sidebarFolders = document.querySelector('#sidebar-folders');
-let sidebarContent = ['Projects', 'Today', 'ThisWeek'];
-
-//Possible remove
-const renderSidebarContent = () => {
-    for (let i = 0; i < sidebarContent.length; i++) {
-        addContent(sidebarFolders, sidebarContent[i], 'sidebar-folder', 'div');
-        addToDoFolder(sidebarContent[i]);
-    }
-    addSidebarInput();
-};
 
 const initSidebar = () => {
-    renderSidebarContent();
+    addSidebarInput();
 };
 
 const addSidebarInput = () => {

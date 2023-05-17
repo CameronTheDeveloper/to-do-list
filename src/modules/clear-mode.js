83fa7a5b761bc8512434ace2da3d840145140b9f
CameronTheDeveloper <cameronposey@outlook.com>
2023-05-16 20:02:26 -0700

    Add all clearmode button styles to clear-mode.css

--- src/modules/clear-mode.js
+++ src/modules/clear-mode.js
@@ -15,21 +15,9 @@ const addClearEventListener = (element) => {
 
 const toggleClearModeButtonStyle = (clearMode) => {
     if (clearMode) {
-        // clearButton.style.boxShadow = '0 0 10px red';
-        // clearButton.style.fontWeight = '900';
-        // clearButton.style.fontSize = '1.4rem';
-        // clearButton.style.color = 'red';
         clearButton.className = 'active';
-        clearButton.style.transition = 'box-shadow 50ms ease-in, ' +
-            'font-weight 50ms ease-in, ' +
-            'font-size 50ms ease-in, ' +
-            'color 50ms ease-in';
 
     } else {
-        clearButton.style.boxShadow = '';
-        clearButton.style.fontWeight = '500';
-        clearButton.style.fontSize = '1.2rem';
-        clearButton.style.color = 'black';
         clearButton.className = 'inactive';
     }
 };
